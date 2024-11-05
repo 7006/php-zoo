86aea1d5c950f97be7344249eb74b7a569f27bce
o.medianyk <o.medianyk@favbet.tech>
2024-11-05 17:21:15 +0200

    Merge AnimalCount and AnimalPopularity classes

--- index.php
+++ index.php
@@ -3,13 +3,11 @@
 use PZ\Db;
 use PZ\EntryCalculator;
 use PZ\Schedule;
-use PZ\AnimalCount;
-use PZ\AnimalPopularity;
+use PZ\Animals;
 
 require_once 'vendor/autoload.php';
 
 $db = Db::load(__DIR__ . '/db.json');
 $calc = new EntryCalculator($db);
 $schedule = new Schedule($db);
-$animals = new AnimalCount($db);
-$popularity = new AnimalPopularity($db);
+$animals = new Animals($db);
