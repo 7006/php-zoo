e65ee388eef4241044179864717cdd5cff3fd147
Oleg Romadin <osromadin@gmail.com>
2024-11-05 19:10:54 +0200

    add byName(...) method

--- src/Animals.php
+++ src/Animals.php
@@ -11,6 +11,22 @@ class Animals
         $this->db = $db;
     }
 
+    public function byName(string $nickName)
+    {   
+        foreach ($this->db->selectAnimals() as $animal) {
+            foreach ($animal['residents'] as $resident) {
+                if ($resident['name'] === $nickName) {
+                    return [
+                        'name' => $resident['name'],
+                        'sex' => $resident['sex'],
+                        'age' => $resident['age'],
+                        'species' => $animal['name'],
+                    ];
+                }
+            }
+        }
+    }
+
     public function byPopularity()
     {
         $byPopularity = [];
