35f6a047fb712d46d10b2bb9b7d944a5ef86555c
Oleg Romadin <osromadin@gmail.com>
2024-11-06 18:42:49 +0200

    add byLocationAndBreedsAndSex() and private filterBySex(...)

--- src/Animals/AnimalMap.php
+++ src/Animals/AnimalMap.php
@@ -7,6 +7,7 @@ trait AnimalMap
 	public function byLocation()
 	{	
 		$byLocation = [];
+		
 		foreach ($this->db->selectAnimals() as $animal) {
 			$byLocation[$animal['location']][] = $animal['name'];
 		}
@@ -18,10 +19,31 @@ trait AnimalMap
 	public function byLocationAndBreeds()
 	{	
 		$byLocationAndBreeds = [];
+		
 		foreach ($this->db->selectAnimals() as $animal) {
 			$byLocationAndBreeds[$animal['location']][$animal['name']] = $this->residentsNickNames($animal['residents']);
 		}
 
 		return $byLocationAndBreeds;
+	}
+
+	//$options = ['sex' => female];
+	public function byLocationAndBreedsAndSex()
+	{	
+		$sex = 'female'; // takes by $options
+		$byLocationAndBreedsAndSex = [];
+				
+		foreach ($this->db->selectAnimals() as $animal) {
+			$residents = $animal['residents'];
+			$filteredResidents = $this->filterBySex($residents, $sex);
+			$byLocationAndBreedsAndSex[$animal['location']][$animal['name']] = $this->residentsNickNames($filteredResidents);
+		}
+
+		return $byLocationAndBreedsAndSex;
+	}
+
+	private function filterBySex(array $residents, string $sex)
+	{
+		return array_filter($residents, fn ($resident) => $resident['sex'] === $sex);
 	}
 }
