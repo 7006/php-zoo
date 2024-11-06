1947955321e3aa6e830e0ce7da981f7f79536020
Oleg Romadin <osromadin@gmail.com>
2024-11-06 18:27:20 +0200

    add byLocationAndBreeds()

--- src/Animals/AnimalMap.php
+++ src/Animals/AnimalMap.php
@@ -13,4 +13,15 @@ trait AnimalMap
 
 		return $byLocation;
 	}
+
+	//$options = ['includeNames' => true];
+	public function byLocationAndBreeds()
+	{	
+		$byLocationAndBreeds = [];
+		foreach ($this->db->selectAnimals() as $animal) {
+			$byLocationAndBreeds[$animal['location']][$animal['name']] = $this->residentsNickNames($animal['residents']);
+		}
+
+		return $byLocationAndBreeds;
+	}
 }
