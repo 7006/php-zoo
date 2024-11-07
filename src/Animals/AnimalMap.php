de70911c61147092a92efc6c515ed3d1ea837245
Oleg Romadin <osromadin@gmail.com>
2024-11-07 17:40:51 +0200

    add filterAviariesBySex([])

--- src/Animals/AnimalMap.php
+++ src/Animals/AnimalMap.php
@@ -4,56 +4,72 @@ namespace PZ\Animals;
 
 trait AnimalMap
 {	
-	public function animalsMap()
-	{
-		return func_num_args() === 0 ? $this->byLocation() : $this->optionsHandler(func_get_arg(0));
-	}
+	// public function animalMap()
+	// {
+	// 	return func_num_args() === 0 ? $this->byLocation() : $this->optionsHandler(func_get_arg(0));
+	// }
 
-	private function optionsHandler(array $options)
-	{
-		$options = func_get_arg(0);
+	// private function optionsHandler(array $options)
+	// {
+	// 	$options = func_get_arg(0);
 		
-		if (isset($options['includeNames']) && $options['includeNames']) {
-			return $this->byLocationName();
-		}
+	// 	if (isset($options['includeNames']) && $options['includeNames']) {
+	// 		return $this->byLocationName();
+	// 	}
 
-		if (isset($options['includeNames']) && $options['includeNames'] && isset($options['sex'])) {
-			return $this->byLocationNameSex($options);
-		}
-	}
+	// 	if (isset($options['includeNames']) && $options['includeNames'] && isset($options['sex'])) {
+	// 		return $this->byLocationNameSex($options);
+	// 	}
+	// }
 
-	public function byLocation()
-	{	
-		$byLocation = [];
+	// public function byLocation()
+	// {	
+	// 	$byLocation = [];
 		
-		foreach ($this->db->selectAnimals() as $animal) {
-			$byLocation[$animal['location']][] = $animal['name'];
-		}
+	// 	foreach ($this->db->selectAnimals() as $animal) {
+	// 		$byLocation[$animal['location']][] = $animal['name'];
+	// 	}
 
-		return $byLocation;
-	}
+	// 	return $byLocation;
+	// }
 
-	public function byLocationName()
-	{	
-		$byLocationName = [];
+	// public function byLocationName()
+	// {	
+	// 	$byLocationName = [];
 		
-		foreach ($this->db->selectAnimals() as $animal) {
-			$byLocationName[$animal['location']][$animal['name']] = $this->residentsNickNames($animal['residents']);
-		}
+	// 	foreach ($this->db->selectAnimals() as $animal) {
+	// 		$byLocationName[$animal['location']][$animal['name']] = $this->residentsNickNames($animal['residents']);
+	// 	}
 
-		return $byLocationName;
-	}
+	// 	return $byLocationName;
+	// }
 
-	public function byLocationNameSex(array $options)
-	{	
-		$options = func_get_arg(0);
-		$byLocationNameSex = [];
+	// public function byLocationNameSex(array $options)
+	// {	
+	// 	$options = func_get_arg(0);
+	// 	$byLocationNameSex = [];
 				
-		foreach ($this->db->selectAnimals() as $animal) {
-			$filteredResidents = $this->filterResidentsBySex($animal['residents'], $options['sex']);
-			$byLocationNameSex[$animal['location']][$animal['name']] = $this->residentsNickNames($filteredResidents);
+	// 	foreach ($this->db->selectAnimals() as $animal) {
+	// 		$filteredResidents = $this->filterResidentsBySex($animal['residents'], $options['sex']);
+	// 		$byLocationNameSex[$animal['location']][$animal['name']] = $this->residentsNickNames($filteredResidents);
+	// 	}
+
+	// 	return $byLocationNameSex;
+	// }
+
+	public function filterAviariesBySex(array $options)
+	{	
+		$filteredAviaries = [];
+		
+		foreach ($this->db->selectAnimals() as $aviary) {
+			$filteredAviaries[] = [
+				'location' => $aviary['location'],
+				'name' => $aviary['name'],
+				'residents' => $this->filterResidentsBySex($aviary['residents'], $options['sex'])
+			];
 		}
 
-		return $byLocationNameSex;
+		return $filteredAviaries;
 	}
 }
+
