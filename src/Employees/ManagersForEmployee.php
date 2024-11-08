c7b4a486ba0f91cb5982d171dc53a0090f3acbde
Oleg Romadin <osromadin@gmail.com>
2024-11-08 19:39:03 +0200

    add check id or name

--- src/Employees/ManagersForEmployee.php
+++ src/Employees/ManagersForEmployee.php
@@ -4,8 +4,14 @@ namespace PZ\Employees;
 
 trait ManagersForEmployee
 {
-	public function managersForEmployee()
+	public function managersForEmployee(string $idOrName)
 	{
-		//return $this->db->selectEmployees();
+		$employees = $this->db->selectEmployees();
+
+		if (str_contains($idOrName, '-')) {
+			return $this->byId($idOrName);
+		} else {
+			return $this->byFirstOrLastName($idOrName);
+		} 
 	}
 }
