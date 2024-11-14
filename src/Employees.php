1b413ab4958789613d1e1719a85bfcaef33a503c
Oleg Romadin <osromadin@gmail.com>
2024-11-14 16:52:18 +0200

    add trait, delete psysh autoload

--- src/Employees.php
+++ src/Employees.php
@@ -5,12 +5,14 @@ namespace PZ;
 use PZ\Employees\EmployeesIds;
 use PZ\Employees\EmployeeName;
 use PZ\Employees\ManagersForEmployee;
+use PZ\Employees\EmployeeCoverage;
 
 class Employees
 {
     use EmployeesIds;
     use EmployeeName;
     use ManagersForEmployee;
+    use EmployeeCoverage;
 
     private $db;
 
---
+++ src/Employees/EmployeeCoverage.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace PZ\Employees;
+
+traint EmployeeCoverage
+{
+
+}
